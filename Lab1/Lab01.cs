ea0113519f236c24d10943a742dcbf67126ee088
Donald <77713078+Dsincennes@users.noreply.github.com>
2023-06-07 20:43:30 -0400

    added lab 1 to new repo

---
+++ Lab1/Lab01.cs
@@ -0,0 +1,179 @@
+﻿/*
+ * Class: CST8359 LAB
+ * Student: Donald Sincennes
+ * Lab: Lab01
+ * Date: May/10/2023
+ */
+
+using System.Diagnostics;
+
+namespace _8359_DonaldSincennes_Lab01;
+
+class Lab01
+{
+    // Check List
+    public static bool CheckList(List<string> wordList)
+    {
+        if (!wordList.Any())
+        {
+            Console.WriteLine("Please import a file first");
+            return false;
+        }
+        return true;
+    }
+
+    // Read words from a file
+    public static List<string> ReadFile()
+    {
+        List<string> wordList = new();
+        try
+        {
+            string[] words = File.ReadAllLines("Words.txt");
+            foreach (string word in words)
+            {
+                wordList.Add(word);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Error reading File.");
+        }
+        Console.WriteLine($"The number of words is {wordList.Count}");
+        return wordList;
+    }
+
+    // Bubble sort words
+    public static void BubbleSort(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        var newList = new List<string>(wordList);
+        Stopwatch sw = new();
+        sw.Start();
+        for (int i = 0; i < newList.Count - 1; i++)
+        {
+            for (int j = 0; j < i - 1; j++)
+            {
+                if (newList[j].CompareTo(newList[j + 1]) > 0)
+                {
+                    (newList[j + 1], newList[j]) = (newList[j], newList[j + 1]); // using tuple to swap
+                }
+            }
+        }
+        sw.Stop();
+        Console.WriteLine($"Bubblesorting the list took {sw.ElapsedMilliseconds} milliseconds.");
+        return;
+    }
+
+    // Linq Sort a list
+    public static void LinqSort(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        Stopwatch sw = new();
+        sw.Start();
+        var sortedNames = wordList.OrderBy(x => x).ToList();
+        sw.Stop();
+        Console.WriteLine($"Linq Sorting the list took {sw.ElapsedMilliseconds} milliseconds.");
+        return;
+    }
+
+    // Count distinct words
+    public static void DistinctCount(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        Console.WriteLine($"Distinct Count is {wordList.Distinct().Count()}");
+        return;
+    }
+
+    // Take last 50 words
+    public static void LastFifty(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        var newList = wordList.SkipWhile((value, index) => index < wordList.Count - 50).ToList();
+        foreach (var word in newList)
+        {
+            Console.WriteLine(word);
+        }
+        return;
+    }
+
+    // Print entire list backwards
+    public static void ListBackwards(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        var newList = new List<string>(wordList);
+        newList.Reverse();
+        foreach (var word in newList)
+        {
+            Console.WriteLine(word);
+        }
+        return;
+    }
+
+    // Display words that end with D, count words.
+    public static void EndsWithD(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        var newList = new List<string>(wordList);
+        int count = 0;
+        foreach (var word in newList.Where(x => x.EndsWith('d')))
+        {
+            Console.WriteLine(word);
+            count++;
+        }
+        Console.WriteLine($"{count} words ended with 'd'");
+    }
+
+    // Display words that start with r, count words.
+    public static void StartsWithR(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        var newList = new List<string>(wordList);
+        int count = 0;
+        foreach (var word in newList.Where(x => x.StartsWith('r')))
+        {
+            Console.WriteLine(word);
+            count++;
+        }
+        Console.WriteLine($"{count} words started with 'r'");
+    }
+
+    // Display words that are bigger then 3 and contain a
+    public static void BiggerThanThreeAndContainA(List<string> wordList)
+    {
+        if (!CheckList(wordList))
+        {
+            return;
+        }
+        var newList = new List<string>(wordList);
+        int count = 0;
+        foreach (var word in newList.Where(x => x.Contains('a') && x.Length > 3))
+        {
+            count++;
+            Console.WriteLine(word);
+        }
+        Console.WriteLine($"{count} words contain 'a' and are greater than 3 in length");
+        return;
+    }
+
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Class: CST8359 LAB
+ * Student: Donald Sincennes
+ * Lab: Lab01
+ * Date: May/10/2023
+ */
+
+using _8359_DonaldSincennes_Lab01;
+
+bool cont = true;
+List<string> list = new();
+
+do
+{
+    Console.Write("""
+        Choose an option:
+        1 - Import Words from File
+        2 - Bubble Sort words
+        3 - LINQ/Lambda sort words
+        4 - Count the Distinct Words
+        5 - Take the last 50 words
+        6 - Reverse print the words
+        7 - Get and display words that end with 'd' and display the count
+        8 - Get and display words that start with 'r' and display the count
+        9 - Get and display words that are more than 3 characters long and include the letter 'a', and display the count
+        x - Exit
+
+        Select an option: 
+        """);
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            list = Lab01.ReadFile();
+            break;
+        case "2":
+            Lab01.BubbleSort(list);
+            break;
+        case "3":
+            Lab01.LinqSort(list);
+            break;
+        case "4":
+            Lab01.DistinctCount(list);
+            break;
+        case "5":
+            Lab01.LastFifty(list);
+            break;
+        case "6":
+            Lab01.ListBackwards(list);
+            break;
+        case "7":
+            Lab01.EndsWithD(list);
+            break;
+        case "8":
+            Lab01.StartsWithR(list);
+            break;
+        case "9":
+            Lab01.BiggerThanThreeAndContainA(list);
+            break;
+        case "x":
+        case "X":
+            Console.WriteLine("Goodbye.");
+            cont = false;
+            break;
+        default:
+            Console.WriteLine("Incorrect Selection. Try Again!");
+            break;
+    }
+} while (cont is not false);
