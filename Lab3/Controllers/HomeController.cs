e336d1f0f773689c1a6ffeaa012e9c1dac612e27
Donald <77713078+Dsincennes@users.noreply.github.com>
2023-06-07 20:41:11 -0400

    adding lab1 / lab3 to a single solution

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing(int numMonkeys)
+        {
+            ViewBag.numMonkeys = numMonkeys;
+
+            return View("Sing", numMonkeys);
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            return View(student);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
