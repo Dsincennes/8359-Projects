bd419213cef2a08a5ec1d89d77692f4366a2fbf7
Donald <77713078+Dsincennes@users.noreply.github.com>
2023-07-06 17:31:35 -0400

    Done up to Controllers Actions and Views. Scaffolding next

--- Lab4/Data/DbInitializer.cs
+++ Lab4/Data/DbInitializer.cs
@@ -1,10 +1,58 @@
-﻿namespace Lab4.Data
+﻿using Lab4.Models;
+
+namespace Lab4.Data
 {
     public static class DbInitializer
     {
         public static void Initialize(SportsDbContext context)
         {
             context.Database.EnsureCreated();
+
+            if (context.Fans.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var fans = new Fan[]
+            {
+            new Fan{FirstName="Carson",LastName="Alexander",BirthDate=DateTime.Parse("1995-01-09")},
+            new Fan{FirstName="Meredith",LastName="Alonso",BirthDate=DateTime.Parse("1992-09-05")},
+            new Fan{FirstName="Arturo",LastName="Anand",BirthDate=DateTime.Parse("1993-10-09")},
+            new Fan{FirstName="Gytis",LastName="Barzdukas",BirthDate=DateTime.Parse("1992-01-09")},
+            };
+            foreach (Fan s in fans)
+            {
+                context.Fans.Add(s);
+            }
+            context.SaveChanges();
+
+            var sportClubs = new SportClub[]
+            {
+            new SportClub{ID="A1",Title="Alpha",Fee=300},
+            new SportClub{ID="B1",Title="Beta",Fee=130},
+            new SportClub{ID="O1",Title="Omega",Fee=390},
+            };
+            foreach (SportClub c in sportClubs)
+            {
+                context.SportClubs.Add(c);
+            }
+            context.SaveChanges();
+
+            var subscriptions = new Subscription[]
+            {
+            new Subscription{FanID=1,SportClubID="A1"},
+            new Subscription{FanID=1,SportClubID="B1"},
+            new Subscription{FanID=1,SportClubID="O1"},
+            new Subscription{FanID=2,SportClubID="A1"},
+            new Subscription{FanID=2,SportClubID="B1"},
+            new Subscription{FanID=3,SportClubID="A1"},
+            };
+            foreach (var subscription in subscriptions)
+            {
+                context.Subscriptions.Add(subscription);
+            }
+            context.SaveChanges();
+
         }
     }
 
